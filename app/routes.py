558d982658a65dc5cd38b8fc041a5d2a5e9b53e7
KalebKeith <kalebkeith@pm.me>
2021-12-13 00:50:28 -0500

    Initial commit for a working flask app. Fresh start.

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Successfully logged in.')
+        return redirect(url_for('home'))
+    return render_template('login.html', form=form)
